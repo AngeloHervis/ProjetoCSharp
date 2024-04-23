d050d005e80914bb8ca2ccbef051e7125443a65e
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-23 15:25:44 -0300

    att

---
+++ WebApi/WebApi/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Model
+{
+    [Table("employee")]
+    public class Employee
+    {
+
+        [Key]
+        public int id { get; private set; }
+        public int name { get; private set; }
+        public int age { get; private set; }
+        public int photo { get; private set; }
+    }
+}
