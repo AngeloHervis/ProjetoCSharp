6d51cc5e1f6e87c8aa8fa42e37d0d1a317a92771
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-25 19:43:53 -0300

    Implantação do CRUD

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,12 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WebAPI.Data;
+using WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurar serviços
+builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=app.db"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<TaskService>();
 
 var app = builder.Build();
+
+// Configurar o pipeline de solicitação
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Definir endpoints
+app.MapControllers();
+
 app.Run();
