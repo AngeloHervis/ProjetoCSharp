f96753566f720708ca8247f16cef0897f0de8db4
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-25 19:31:48 -0300

    finalmente compilou <3

--- WebAPI/Models/Task.cs
+++ WebAPI/Models/Task.cs
@@ -3,8 +3,8 @@ namespace WebAPI.Models
     public class Task
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
         public bool IsCompleted { get; set; }
     }
 }
--- WebAPI/Models/User.cs
+++ WebAPI/Models/User.cs
@@ -3,7 +3,7 @@ namespace WebAPI.Models
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
     }
 }
