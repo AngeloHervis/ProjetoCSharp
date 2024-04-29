a636e9371ba246574fb7dd2b483cb9e953d17a3e
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-29 18:47:11 -0300

    iniciando testes

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2ed352762063889a1ba74b205f4bee1fb0b124")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af63ef97cf678e45e01dba238b3ac37a88db87a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
