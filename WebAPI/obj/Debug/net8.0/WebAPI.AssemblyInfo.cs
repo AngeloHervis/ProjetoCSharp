d31509291b4f5a2584fffd3295e9f2cc43c7839a
NicolasGasparetto <167822500+NicolasGasparetto@users.noreply.github.com>
2024-05-14 09:40:26 -0300

    .

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94c541977047b313753196fef92487c7a62178a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55a7039c5e38f340e32619d9af698d33175c7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
