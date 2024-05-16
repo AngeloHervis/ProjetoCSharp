d35de34e2202f90c0e8774dd3635caf934585910
NicolasGasparetto <167822500+NicolasGasparetto@users.noreply.github.com>
2024-05-16 20:59:47 -0300

    v2.0

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+namespace ListaDeTarefas.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Tarefa> Tarefas { get; set; }
+    public DbSet<Usuario> Usuarios{ get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=ListaTarefas.db");
+    }
+}
